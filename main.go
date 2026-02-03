6c7c9939dd25cd82f36c07b853a3c78aa9279634
Dhanalakshmi D <dhanalakshmid.cse2023@citchennai.net>
2026-02-03 15:23:39 +0530

    LRU cache via Interface

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/dhanalakshms/multi-backend-cache-go/cache"
 	"github.com/dhanalakshms/multi-backend-cache-go/inmemory"
 )
 
@@ -11,43 +12,44 @@ func main() {
 
 	fmt.Println("===== LRU Cache test =====")
 
-	cache := inmemory.NewLRUCache(2)
+	var c cache.Cache
+	c = inmemory.NewLRUCache(2)
 
 	fmt.Println("\n1) Basic SET + GET")
-	cache.Set("a", "apple", 0)
+	c.Set("a", "apple", 0)
 
-	val, ok := cache.Get("a")
+	val, ok := c.Get("a")
 	fmt.Println("Get a:", val, ok)
 
 	fmt.Println("\n2) LRU Eviction")
-	cache.Set("b", "Mango", 0)
-	cache.Set("c", "Orange", 0) // should evict "a"
+	c.Set("b", "Mango", 0)
+	c.Set("c", "Orange", 0) /
 
-	_, ok = cache.Get("a")
+	_, ok = c.Get("a")
 	fmt.Println("a exists after eviction:", ok)
 
 
 	fmt.Println("\n3) Delete test")
-	cache.Set("x", "delete-me", 0)
+	c.Set("x", "delete-me", 0)
 
-	cache.Delete("x")
-	_, ok = cache.Get("x")
+	c.Delete("x")
+	_, ok = c.Get("x")
 	fmt.Println("x exists after delete:", ok)
 
 
 	fmt.Println("\n4) TTL expiry test")
-	cache.Set("ttl", "sample", 2*time.Second)
+	c.Set("ttl", "sample", 2*time.Second)
 
-	val, ok = cache.Get("ttl")
+	val, ok = c.Get("ttl")
 	fmt.Println("before expiry:", val, ok)
 
 	time.Sleep(3 * time.Second)
 
-	_, ok = cache.Get("ttl")
+	_, ok = c.Get("ttl")
 	fmt.Println("after expiry:", ok)
 
 	fmt.Println("\n5) Missing key test")
-	_, ok = cache.Get("unknown")
+	_, ok = c.Get("unknown")
 	fmt.Println("unknown exists:", ok)
 
 
